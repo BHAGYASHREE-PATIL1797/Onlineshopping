7074f4d70b53e9fd71431fe8704125ae2ce6f489
BHAGYASHREE-PATIL1797 <73108747+BHAGYASHREE-PATIL1797@users.noreply.github.com>
2020-10-20 15:57:31 +0530

    Update home.php

--- home.php
+++ home.php
@@ -7,6 +7,7 @@
     while($row = mysqli_fetch_assoc($result))
     {
         //echo("<a href='selectproduct.php?catid=".$row["c_id"]."'>".$row["c_name"]."</a> <br/>" );
+        //github
 echo("<a href='selectproduct.php?catid=".$row["c_id"]."'>".$row["c_name"]."</a> <br/>" );
     }
     mysqli_close($con);
